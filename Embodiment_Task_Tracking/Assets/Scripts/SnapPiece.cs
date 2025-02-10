88417986d122043bd1002cf95ab2d6a2de175eac
Juanita Benjamin <JuanitaBenjamin98@gmail.com>
2025-02-10 12:00:13 -0500

    deleted SteamVR and added Avatars where we can disable the lower body

---
+++ Embodiment_Task_Tracking/Assets/Scripts/SnapPiece.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapPiece : MonoBehaviour
+{
+    public string tag;
+    Collider collider;
+    AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = GetComponent<Collider>();
+        collider.isTrigger = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(tag)) { 
+
+            //set position
+            other.gameObject.transform.position = gameObject.transform.position;
+            other.gameObject.transform.rotation = gameObject.transform.rotation;
+
+            InitAudio(other);
+
+            source.Play();
+
+            //then stop the physics?
+            Rigidbody body = other.gameObject.GetComponent<Rigidbody>();
+            body.velocity = Vector3.zero;
+            body.angularVelocity = Vector3.zero;
+
+            body.constraints = RigidbodyConstraints.FreezeAll;
+            body.isKinematic = true;
+
+        }
+
+    }
+
+    private void InitAudio(Collider other)
+    {
+        source = other.gameObject.AddComponent<AudioSource>();
+        source.clip = Resources.Load("Sounds/Pop") as AudioClip;
+        source.pitch = 0.9f;
+        source.spatialBlend = 1f;
+    }
+
+}
