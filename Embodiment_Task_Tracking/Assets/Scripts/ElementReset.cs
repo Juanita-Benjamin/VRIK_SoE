d7ce2818825e6cacc99b2415839cf8c21285a6bd
Juanita Benjamin <JuanitaBenjamin98@gmail.com>
2025-06-19 17:09:09 -0400

    modified Body tracking to 'High'

--- Embodiment_Task_Tracking/Assets/Scripts/ElementReset.cs
+++ Embodiment_Task_Tracking/Assets/Scripts/ElementReset.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using Oculus.Movement.BodyTrackingForFitness;
 
 public class ElementReset : MonoBehaviour
 {
